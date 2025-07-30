f4692df718761c95db5daf3aab7bdef5514a5963
Liam Pilson <Liamphone0@gmail.com>
2025-07-29 21:42:14 -0400

    Changes name of downloaded file in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -o Default.code-profile https://cdn.klns.ca/static/1f3008de-5c43-44d2-af50-c5a8a1dd2c6c.code-profile
+curl -o liam.code-profile https://cdn.klns.ca/static/1f3008de-5c43-44d2-af50-c5a8a1dd2c6c.code-profile
